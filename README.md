045d52cf51cb2311b23e6db668caec7fcce5c12a
AlainGourves <alain.gourves@me.com>
2021-06-23 16:43:34 +0200

    SVG icon system

--- README.md
+++ README.md
@@ -26,7 +26,51 @@ git branch -M main
 gulp
 ```
 Usually the server starts at : [http://localhost:3001/](http://localhost:3002/)
-## Gulp
+
+## SVG sprites
+
+Individual icon files are placed in the `/icons/` folder. The name of each file, prefixed with `icon-`, sets the `id` of the associated `<symbol>`.
+
+To generate the sprite (`/img/icons-sprite.svg`) :
+
+```bash
+gulp svg
+```
+Sprite's generation is included in the default task : adding a SVG file in the `/icons/` folder leads to a brand new ~~cadillac~~ sprite.
+
+### `svgstore` : generate SVG sprites
+
+[GitHub Repository](https://github.com/w0rm/gulp-svgstore)
+
+The following options are set automatically based on file data:
+
+- `id` attribute of the `<symbol>` element is set to the name of corresponding file
+- result filename is the __name of base directory of the first file__.
+
+If your workflow is different, please use `gulp-rename` to rename sources or result.
+
+The only available option is:
+
+- `inlineSvg` — output only `<svg>` element without `<?xml ?>` and `DOCTYPE` to use inline, __default: `false`__.
+
+### SVG Optimization with `svgmin`
+
+- [GitHub Repository](https://github.com/ben-eb/gulp-svgmin)
+
+__Note__ Relies on [`SVGO`](https://github.com/svg/svgo), which is globally installed.
+
+### Remove unnecessary `fill` attributes
+
+Thanks to [gulp-cheerio](https://github.com/knpwrs/gulp-cheerio)
+
+[Cheerio](https://github.com/cheeriojs/cheerio) is an implementation of jQuery for the server, it makes it easy to remove SVG attributes : 
+
+```javascript
+$("[fill]").removeAttr("fill");
+```
+__NB__: It's also possible to replace the value of every `fill` attribute with `currentColor`. Thus the `fill` color of the path is controlled via CSS with the `color` property of the parent `svg`.
+
+## Links
 
 Installed plugins :
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const svgstore = require('gulp-svgstore');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
 
 const reload = browserSync.reload;
 
@@ -20,6 +23,36 @@ function style() {
         .pipe(gulp.dest('./css'));
 }
 
+// SVG sprite
+function svgSprite() {
+    return gulp.src('./icons/*.svg')
+        .pipe(rename({ prefix: 'icon-' }))
+        .pipe(svgmin({
+                multipass: true,
+                plugins: [
+                    {
+                        removeViewBox: false,
+                        removeDimensions: true
+                    }
+                ]
+            }
+        ))
+        .pipe(svgstore())
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                // $('[fill]').each(function() {
+                //     if ($(this).attr('fill') !== 'currentColor' || $(this).attr('fill') !== 'currentcolor') {
+                //         $(this).attr('fill', 'currentColor');
+                //     }
+                // });
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(rename('icons-sprite.svg'))
+        .pipe(gulp.dest('./img'));
+}
+
 // Watch
 // server: true     -> serve files from the current directory
 // open: false      -> stop the browser from automatically opening
@@ -31,10 +64,12 @@ function watch() {
         open: false,
         ghostMode: false
     });
+    gulp.watch('./icons/*.svg', svgSprite);
     gulp.watch('./scss/**/*.scss', style)
     gulp.watch('./css/**/*.css').on('change', reload);
     gulp.watch('./*.html').on('change', reload);
     gulp.watch('./js/**/*.js').on('change', reload);
 };
 
+exports.svg = svgSprite;
 exports.default = watch;
